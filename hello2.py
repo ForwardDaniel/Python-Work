cc3fa4d63122ad131fc9dfce9905a32c7e2ec959
ForwardDaniel <danielondieki@gmail.com>
2018-07-17 11:42:15 +0300

    First Commit

---
+++ hello2.py
@@ -0,0 +1,21 @@
+x=10
+print(x)
+y=30
+print(y)
+z=x+y
+print(z)
+print (x+y)
+print (z+x)
+
+
+p=8000
+r=10
+t=2
+A = (p*r*t)/100
+print (A)
+
+weight = 60
+height = 236
+age = 24
+bmr = 66 + (6.2 * weight) + (12.7 * height) - (6.76 * age)
+print(bmr)
